ad2ddd737ec2510af9faf70018bad5d06fd707d0
Jen Lucas <36136983+jcl6701@users.noreply.github.com>
2019-10-16 15:17:16 -0400

    initial commit

---
+++ Physics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start() {
+        // init world state
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    
+}
---
+++ PhysicsObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : Object {
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void OnMouseDown() {
+        // start registering
+        Debug.Log("Touched");
+    }
+
+    void OnMouseUp() {
+        // stop registering
+        Debug.Log("Untouched");
+    }
+
+}
