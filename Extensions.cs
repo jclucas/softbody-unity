18e8303a31d442627de167219bfd77278a4afd77
Jen Lucas <36136983+jcl6701@users.noreply.github.com>
2019-11-11 23:27:15 -0500

    RK4 integrator

--- Extensions.cs
+++ Extensions.cs
@@ -4,25 +4,28 @@ using System.Collections.Generic;
 
 public static class Extensions {
 
-    public static Particle[] Step(this Particle[] state, Force f, float dt) {
+    public static ParticleState[] Step(this Particle[] state, Force f, float dt) {
 
-        var deriv = new ParticleState[state.Length];
+        var next = new ParticleState[state.Length];
         
         for (int i = 0; i < state.Length; i++) {
-            deriv[i] = new ParticleState();
+            next[i] = new ParticleState();
             // step forward with current derivatives
-            deriv[i].position = state[i].position + state[i].velocity * dt;
-            deriv[i].velocity = state[i].velocity + state[i].force / state[i].mass * dt;
+            next[i].position = state[i].position + state[i].velocity * dt;
+            next[i].velocity = state[i].velocity + state[i].force / state[i].mass * dt;
             // calculate
-            deriv[i].force = f.eval(i, state, dt);
-            state[i].SetState(deriv[i]);
+            next[i].force = f.eval(i, state, dt);
+        }
+
+        for (int i = 0; i < state.Length; i++) {
+            state[i].SetState(next[i]);
         }
         
-        return state;
+        return next;
 
     }
 
-    public static Particle[] IntegrateMidpoint(this Particle[] state, Force f, float dt) {
+    public static ParticleState[] IntegrateMidpoint(this Particle[] state, Force f, float dt) {
         var k1 = state.Step(f, dt / 2);
         return state.Step(f, dt);
     }
@@ -30,9 +33,15 @@ public static class Extensions {
     public static Particle[] Integrate(this Particle[] state, Force f, float dt) {
         
         var k1 = state.Step(f, 0);
-        var k2 = k1.Step(f, dt/2);
-        var k3 = k2.Step(f, dt/2);
-        var k4 = k3.Step(f, dt);
+        var k2 = state.Step(f, dt/2);
+        var k3 = state.Step(f, dt/2);
+        var k4 = state.Step(f, dt);
+
+        for (int p = 0; p < state.Length; p++) {
+            state[p].position = ((k1[p].position + (k2[p].position + k3[p].position) * 2 + k4[p].position) * (1f/6f));
+            state[p].velocity = ((k1[p].velocity + (k2[p].velocity + k3[p].velocity) * 2 + k4[p].velocity) * (1f/6f));
+            state[p].force = ((k1[p].force + (k2[p].force + k3[p].force) * 2 + k4[p].force) * (1f/6f));
+        }
 
         return state;
 
